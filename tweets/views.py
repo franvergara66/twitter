bb077441fef0181fb90f3f28e9622ee2bcac76ec
Francisco Vergara <javiervergara2004@gmail.com>
2020-06-01 00:55:43 +0200

    add: templates folder

--- tweets/views.py
+++ tweets/views.py
@@ -4,11 +4,12 @@ from .models import Tweet
 
 def home_view(request, *args, **kwargs):
     print(args, kwargs)
-    return HttpResponse("<h1>Hello World</h1>")   
+    #return HttpResponse("<h1>Hello World</h1>") 
+    return render(request, "pages/home.html", context={}, status=200)  
 
 def tweet_detail_view(request, tweed_id, *args, **kwargs):
     """
-    REST API VIEW 
+    REST API VIEW python
         Consume by javascript, or Swift/Java/iOS/Android
         return json.data
     """
--- twitter/settings.py
+++ twitter/settings.py
@@ -13,9 +13,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# Remember on the same level as manage.py
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+print(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'twitter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

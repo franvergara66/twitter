4fd506370ce1bd2d4b2db5074da7c803a16e5863
Francisco Vergara <javiervergara2004@gmail.com>
2020-06-01 19:57:08 +0200

    add: Format Tweet Method

--- tweets/views.py
+++ tweets/views.py
@@ -2,6 +2,22 @@ from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 from .models import Tweet
 
+def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW 
+    Consume by javascript, or Swift/Java/iOS/Android
+    return json.data
+    """
+    qs = Tweet.objects.all()
+    tweet_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+    "isUser": False, 
+        "response": tweet_list,
+ 
+    }
+    return JsonResponse(data)
+
+
 def home_view(request, *args, **kwargs):
     print(args, kwargs)
     #return HttpResponse("<h1>Hello World</h1>") 
@@ -9,9 +25,9 @@ def home_view(request, *args, **kwargs):
 
 def tweet_detail_view(request, tweed_id, *args, **kwargs):
     """
-    REST API VIEW python
-        Consume by javascript, or Swift/Java/iOS/Android
-        return json.data
+    REST API VIEW 
+    Consume by javascript, or Swift/Java/iOS/Android
+    return json.data
     """
     data = {
         "id": tweed_id,
